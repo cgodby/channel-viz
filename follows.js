479501b0ebec8b52e4cfc41c752fd17ce4c283b5
cgodby <cgodby@users.noreply.github.com>
2014-07-29 10:38:31 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= 'wZ2b6vhzPhbYFqUJYU3Cd96zm1QVWLzss7jOaeXHU7CcRf95', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [2021515235], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Remote Monitoring of Stand-Alone PV System ', // Replaces Xively logo in the header
+		applicationName	= 'GSM/GPRS Remote Monitoring of Stand-Alone PV System ', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
