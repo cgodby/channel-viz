b3d3c63918a41517bbe4ef9929ca936ddf4d3aed
cgodby <cgodby@users.noreply.github.com>
2014-07-29 10:22:09 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,12 +12,12 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+	var defaultKey		= 'wZ2b6vhzPhbYFqUJYU3Cd96zm1QVWLzss7jOaeXHU7CcRf95', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [2021515235], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Remote Monitoring of Stand-Alone PV System ', // Replaces Xively logo in the header
+		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
+		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
